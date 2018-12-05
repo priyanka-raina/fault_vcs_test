8a2ef5b72455ea9dcbef2c6a352a415403eef8d8
Priyanka Raina <praina@rice11.stanford.edu>
2018-12-04 20:18:54 -0800

    Fix vcs simulation

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# fault_vcs_test
+To run simulation with verilator
+```
+python counter.py | python tester_counter.py
+```
 
-To run `python counter.py | python tester_counter.py`
+To run simulation with vcs
+```
+vcs -sverilog -full64 +v2k -timescale=1ns/1ns verilog_tester_counter.v ./build/Counter4_COUT.v
+./simv
+```
--- verilog_tester_counter.v
+++ verilog_tester_counter.v
@@ -12,22 +12,53 @@ module verilog_tester ();
 
   initial begin
     CLK = 1'b0;
-  end
+    // Arbitrarily taking clock period of 10
+    #5 CLK = ~CLK; // 1
+    $display("%t O = %h", $time, O);
+    $display("%t COUT = %h", $time, COUT);
+    assert(O == 4'b0000);
+    assert(COUT == 1'b0);
 
-  task runtest;
-    begin
-      // Arbitrarily taking clock period of 10
-      #5 CLK = ~CLK; // 1
-      assert(O == 4'b0000);
-      assert(COUT == 1'b0);
-      #5 CLK = ~CLK; // 0
-      #5 CLK = ~CLK; // 1
-      assert(O == 4'b0001);
-      assert(COUT == 1'b0);
-      #5 CLK = ~CLK; // 0
-      #5 CLK = ~CLK; // 1
-      assert(O == 4'b0001);
-      assert(COUT == 1'b0);
-    end
-  endtask
+    #5 CLK = ~CLK; // 0
+    #5 CLK = ~CLK; // 1
+    $display("%t O = %h", $time, O);
+    $display("%t COUT = %h", $time, COUT);
+    assert(O == 4'b0001);
+    assert(COUT == 1'b0);
+
+    #5 CLK = ~CLK; // 0
+    #5 CLK = ~CLK; // 1
+    $display("%t O = %h", $time, O);
+    $display("%t COUT = %h", $time, COUT);
+    assert(O == 4'b0010);
+    assert(COUT == 1'b0);
+
+    #5 CLK = ~CLK; // 0
+    #5 CLK = ~CLK; // 1
+    $display("%t O = %h", $time, O);
+    $display("%t COUT = %h", $time, COUT);
+    assert(O == 4'b0011);
+    assert(COUT == 1'b0);
+
+    #5 CLK = ~CLK; // 0
+    #5 CLK = ~CLK; // 1
+    $display("%t O = %h", $time, O);
+    $display("%t COUT = %h", $time, COUT);
+    assert(O == 4'b0100);
+    assert(COUT == 1'b0);
+
+    #5 CLK = ~CLK; // 0
+    #5 CLK = ~CLK; // 1
+    $display("%t O = %h", $time, O);
+    $display("%t COUT = %h", $time, COUT);
+    assert(O == 4'b0101);
+    assert(COUT == 1'b0);
+
+    #5 CLK = ~CLK; // 0
+    #5 CLK = ~CLK; // 1
+    $display("%t O = %h", $time, O);
+    $display("%t COUT = %h", $time, COUT);
+    assert(O == 4'b0110);
+    assert(COUT == 1'b0);
+  end
 endmodule
